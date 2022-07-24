9bed68b8673cb4266e5c9486a3030a063c535005
Santosh <santoshkushwah01012001@gmail.com>
2022-07-24 18:25:23 +0530

    First commit React app

---
+++ src/MyComponents/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      <h3>This is About Component</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia fugiat
+        numquam assumenda cumque! Necessitatibus cumque explicabo, sed,
+        praesentium accusamus repellat molestiae corporis veniam ad incidunt
+        magnam iusto, quae ex quibusdam. Ullam, vel dolorem! Quidem.
+      </p>
+    </div>
+  );
+}
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className='bg-dark text-light my-3'>
+      <p className="text-center py-3">
+        copyright &copy; MyTodoList.com
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
